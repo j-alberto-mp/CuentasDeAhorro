a23f51464b14c4d503a86b538ef75778672aa417
Jose Alberto Martinez Pliego <j.alberto_mp@outlook.com>
2022-09-11 18:24:44 -0500

    Se agrega archivo js para las peticiones http

---
+++ CuentasAhorro.UI/wwwroot/js/repository.js
@@ -0,0 +1,31 @@
+﻿class Repository {
+    constructor(controller) {
+        this.controller = controller;
+    }
+
+    httpGet(action, params) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                method: 'GET',
+                url: `/${this.controller}/${action}`,
+                data: params,
+                dataType: 'JSON',
+                success: resolve,
+                error: reject
+            });
+        });
+    };
+
+    httpPost(action, params) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                method: 'POST',
+                url: `/${this.controller}/${action}`,
+                data: params,
+                dataType: 'JSON',
+                success: resolve,
+                error: reject
+            });
+        });
+    }
+}

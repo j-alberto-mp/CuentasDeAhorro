7dbe8da712994f69f8de6d8f6e92ef15ebe88c20
Jose Alberto Martinez Pliego <j.alberto_mp@outlook.com>
2022-09-11 19:01:22 -0500

    Se agrega libería de Swet Alert para mostrar notificaciones

--- CuentasAhorro.UI/wwwroot/js/site.js
+++ CuentasAhorro.UI/wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿$(function () {
+﻿const waiting = () => {
+    swal({
+        icon: 'info',
+        title: 'Espera un momento',
+        text: 'Consultando la información, esto puede demorar un poco...',
+        buttons: false,
+        closeOnClickOutside: false,
+        closeOnEsc: false
+    });
+};
+
+const success = (title, text, redirect) => {
+    swal({
+        icon: 'success',
+        title: title,
+        text: text,
+        buttons: false,
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        timer: 2500,
+    }).then(() => {
+        if (redirect) {
+            window.location.href = redirect;
+        }
+    });
+};
+
+const serviceError = (text) => {
+    swal({
+        icon: 'error',
+        title: 'Ocurrió un error en el servidor',
+        text: text,
+        buttons: false,
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        timer: 2500,
+    });
+};
+
+const requestError = (text) => {
+    swal({
+        icon: 'warning',
+        title: 'Ocurrió un error en la solicitud',
+        text: text,
+        buttons: false,
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        timer: 2500,
+    });
+};
+
+$(function () {
     let collapsed = sessionStorage.getItem('collapsed');
 
     if (collapsed === 'closed') {

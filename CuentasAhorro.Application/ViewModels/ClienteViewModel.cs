9ff44890deacbb2104e4b01e3f5748a360f1e4c7
Jose Alberto Martinez Pliego <j.alberto_mp@outlook.com>
2022-09-11 22:06:44 -0500

    Ajustes en el viewmodel del cliente

--- CuentasAhorro.Application/ViewModels/ClienteViewModel.cs
+++ CuentasAhorro.Application/ViewModels/ClienteViewModel.cs
@@ -6,8 +6,8 @@
         public string Nombre { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
-        public string UsuarioAltaId { get; set; }
+        public DateTime FechaRegistro { get; set; }
 
-        public string NombreCompeto { get { return $"{Nombre} {ApellidoPaterno} {ApellidoMaterno}"; } }
+        public string NombreCompleto { get { return $"{Nombre} {ApellidoPaterno} {ApellidoMaterno}"; } }
     }
 }

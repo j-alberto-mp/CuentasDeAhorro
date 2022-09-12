0e5c08eba66c444cf74f0d807ee803a8af4fcbc3
Jose Alberto Martinez Pliego <j.alberto_mp@outlook.com>
2022-09-12 00:46:58 -0500

    Ajustes en aviso de espera

--- CuentasAhorro.UI/wwwroot/js/cliente/detalles.js
+++ CuentasAhorro.UI/wwwroot/js/cliente/detalles.js
@@ -1,50 +1,63 @@
 ﻿const clienteRepository = new Repository('Cliente');
 const cuentaRepository = new Repository('Cuenta');
 
+let obtenerCliente = () => {
+    clienteRepository.httpGet('ObtenerDetalles', { id: $('#clienteID').val() })
+        .then((response) => {
+            if (response.correct) {
+                $('#nombre').html(response.data.nombreCompleto);
+                $('#numero').html(`<b>${response.data.clienteID}</b>`);
+                $('#fecha').html(`<b>${moment(response.data.fechaRegistro).format('YYYY-MM-DD')}</b>`);
+
+                obtenerCuentas();
+            }
+            else {
+                requestError(response.message);
+            }
+        })
+        .catch((error) => {
+            serviceError(`${error.status} - ${error.statusText}`);
+        });
+};
+
+let obtenerCuentas = () => {
+    $('.cards').empty();
+
+    cuentaRepository.httpGet('ObtenerCuentas', { id: $('#clienteID').val() })
+        .then((response) => {
+            if (response.correct) {
+                if (response.data[0]) {
+                    $.each(response.data, function (key, value) {
+                        $('.cards').append(`<div class="cards__card" data-cuenta="${value.cuentaID}"><label><i class="fi fi-rr-wallet"></i></label><p>${value.numeroCuenta}</p></div>`);
+                    })
+                }
+                else {
+                    $('.cards').append('<div class="cards__card--empty"><i class="fi fi-rr-engine-warning"></i><h6>El cliente aún no ha aperturado ninguna cuenta de ahorro</h5></div>');
+                }
+
+                swal.close();
+            }
+            else {
+                requestError(response.message);
+            }
+        })
+        .catch((error) => {
+            serviceError(`${error.status} - ${error.statusText}`);
+        });
+}
+
 $(function () {
     moment.tz.setDefault('America/Mexico_City');
 
-    waiting();
-
     if ($('#clienteID').val()) {
-        clienteRepository.httpGet('ObtenerDetalles', { id: $('#clienteID').val() })
-            .then((response) => {
-                if (response.correct) {
-                    $('#nombre').html(response.data.nombreCompleto);
-                    $('#numero').html(`<b>${response.data.clienteID}</b>`);
-                    $('#fecha').html(`<b>${moment(response.data.fechaRegistro).format('YYYY-MM-DD')}</b>`);
+        waiting();
 
-                    $('.cards').empty();
-
-                    cuentaRepository.httpGet('ObtenerCuentas', { id: $('#clienteID').val() })
-                        .then((response) => {
-                            if (response.correct) {
-                                if (response.data[0]) {
-                                    $.each(response.data, function (key, value) {
-                                        $('.cards').append(`<div class="cards__card" data-cuenta="${value.cuentaID}"><label><i class="fi fi-rr-wallet"></i></label><p>${value.numeroCuenta}</p></div>`);
-                                    })
-                                }
-                                else {
-                                    $('.cards').append('<div class="cards__card--empty"><i class="fi fi-rr-engine-warning"></i><h6>El cliente aún no ha aperturado ninguna cuenta de ahorro</h5></div>');
-                                }
-
-                                swal.close();
-                            }
-                            else {
-                                requestError(response.message);
-                            }
-                        })
-                        .catch((error) => {
-                            serviceError(`${error.status} - ${error.statusText}`);
-                        });
-                }
-                else {
-                    requestError(response.message);
-                }
-            })
-            .catch((error) => {
-                serviceError(`${error.status} - ${error.statusText}`);
-            });
+        setTimeout(function () {
+            obtenerCliente();
+        }, 1500);
+    }
+    else {
+        $('#aperturar').prop('disabled', true);
     }
 
     $('#aperturar').on('click', function () {
